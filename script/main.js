81f43e6a32e5a4b994a8a917f4f07a6450faeb55
911-duck <zhenyabiruk800@gmail.com>
2025-11-09 21:25:56 +0300

    add branch add-notes -modified style.css -modified index.html -modified main.js -add folder img with mountains.png

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
 // !code here 
+
+// ________________create-note________________
+
+const MAIN = document.querySelector(".main");
+const HEADER_SETTINGS = document.querySelector(".note-header-settings-screen");
+
+const BUTTON_ADD_NOTE_HEADER = document.querySelector(".menu_add-note-header");
+const BUTTON_RESET_NOTE_HEADER = document.querySelector(".note-header_exit");
+
+function closeNoteSettings(event){
+    BUTTON_RESET_NOTE_HEADER.removeEventListener('click',closeNoteSettings)
+    MAIN.style.transform = `translateY(0%)`
+    HEADER_SETTINGS.style.transform = `translateY(120%)`
+    BUTTON_ADD_NOTE_HEADER.addEventListener('click',openNoteSettings)
+}
+
+function openNoteSettings(event){
+    BUTTON_ADD_NOTE_HEADER.removeEventListener('click',openNoteSettings)
+    MAIN.style.transform = `translateY(-${MAIN.offsetHeight}px)`
+    HEADER_SETTINGS.style.transform = `translateY(0px)`
+    BUTTON_RESET_NOTE_HEADER.addEventListener('click',closeNoteSettings)
+}
+
+BUTTON_ADD_NOTE_HEADER.addEventListener('click',openNoteSettings)
