e333195ab88a8e855a17e0c4d5e308b55f3f5c7c
unknown <e.kirieshkajava@gmail.com>
2025-11-14 16:30:11 +0300

    added animation to settings

--- script/main.js
+++ script/main.js
@@ -1,1 +1,14 @@
-// !code here 
+const setBlock = document.querySelector('.settings-block')
+const settings = document.querySelector('.setting')
+setBlock.onclick = ()=> {
+    settings.style.animation = 'animation: ToLeft 0.5s linear forwards;'
+    setBlock.classList.replace('settings-block', 'false')
+    settings.classList.replace('false', 'settings')
+    settings.style.left = '0px'
+}
+settings.onclick = ()=> {
+    settings.style.animation = 'animation: ToRight 0.5s linear forwards;'
+    setBlock.classList.replace('false', 'settings-block')
+    settings.classList.replace('settings', 'false')
+    settings.style.left = '-200px'
+}
