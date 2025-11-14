537bb18a0d7770ef34d6bda6ad8e8b77e268cf59
unknown <e.kirieshkajava@gmail.com>
2025-11-14 20:23:45 +0300

    remake visual to settings

--- script/main.js
+++
@@ -1,7 +0,0 @@
-const settingIconBlock = document.querySelector('.menu_settings-icon')
-const settings = document.querySelector('.setting')
-settingIconBlock.onclick = ()=> {
-    settingIconBlock.classList.toggle('false')
-    settings.classList.toggle('settings')
-    settings.style.left = '0px'
-}
